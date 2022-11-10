cbd04e3157e0ccc782c2278960028316d7212c20
jisunchung <syi06117@gmail.com>
2022-11-10 22:50:03 +0900

    add scrollview in filter

--- Screen/Filter.js
+++ Screen/Filter.js
@@ -1,5 +1,14 @@
 import React, { useEffect } from "react";
-import { StyleSheet, Text, View, Button, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Dimensions,
+  StatusBar,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
 import { useState } from "react";
 import { MultipleSelectList } from "react-native-dropdown-select-list";
 import Slider from "@react-native-community/slider";
@@ -22,51 +31,58 @@ function Filter({ navigation }) {
   ];
 
   return (
-    <View style={styles.container}>
-      <Text>테마</Text>
-      <MultipleSelectList
-        setSelected={(val) => setTheme(val)}
-        data={themeItems}
-        save="value"
-        // onSelect={() => alert(selected)}
-        label="테마"
-      />
-      <Text>{theme}</Text>
-      <Text>인구수</Text>
-      <Slider
-        style={styles.slider}
-        value={population}
-        onValueChange={(val) => setPopulation(val)}
-        minimumValue={0}
-        maximumValue={1000}
-        minimumTrackTintColor="#44AD5E"
-        maximumTrackTintColor="#FFFFFF"
-        step={1}
-      />
-      <Text>{population} ~ 1000</Text>
-      <Text>거리</Text>
-      <Slider
-        style={styles.slider}
-        value={distance}
-        onValueChange={(val) => setDistance(val)}
-        minimumValue={0}
-        maximumValue={500}
-        minimumTrackTintColor="#44AD5E"
-        maximumTrackTintColor="#FFFFFF"
-        step={10}
-      />
-      <Text>{distance}km</Text>
-      <Text>접근성</Text>
-      <MultipleSelectList
-        setSelected={(val) => setAccess(val)}
-        data={accessItems}
-        save="value"
-        // onSelect={() => alert(selected)}
-        label="접근성"
-      />
-      <Text>{access}</Text>
-      <Button title="submit" onPress={() => navigation.navigate("Result")} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.box}>
+          <Text>테마</Text>
+          <MultipleSelectList
+            setSelected={(val) => setTheme(val)}
+            data={themeItems}
+            save="value"
+            // onSelect={() => alert(selected)}
+            label="테마"
+          />
+          <Text>{theme}</Text>
+          <Text>인구수</Text>
+          <Slider
+            style={styles.slider}
+            value={population}
+            onValueChange={(val) => setPopulation(val)}
+            minimumValue={0}
+            maximumValue={1000}
+            minimumTrackTintColor="#44AD5E"
+            maximumTrackTintColor="#FFFFFF"
+            step={1}
+          />
+          <Text>{population} ~ 1000</Text>
+          <Text>거리</Text>
+          <Slider
+            style={styles.slider}
+            value={distance}
+            onValueChange={(val) => setDistance(val)}
+            minimumValue={0}
+            maximumValue={500}
+            minimumTrackTintColor="#44AD5E"
+            maximumTrackTintColor="#FFFFFF"
+            step={10}
+          />
+          <Text>{distance}km</Text>
+          <Text>접근성</Text>
+          <MultipleSelectList
+            setSelected={(val) => setAccess(val)}
+            data={accessItems}
+            save="value"
+            // onSelect={() => alert(selected)}
+            label="접근성"
+          />
+          <Text>{access}</Text>
+          <Button
+            title="submit"
+            onPress={() => navigation.navigate("Result")}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 const screenWidth = Dimensions.get("window").width;
@@ -74,8 +90,14 @@ const screenHeight = Dimensions.get("window").height;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    // backgroundColor: "pink",
+  },
+  box: {
     paddingHorizontal: 20,
-    paddingVertical: 20,
+    // paddingVertical: 20,
   },
   slider: {
     width: screenWidth - 40,
