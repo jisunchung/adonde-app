8f259137ce338d995a3c09ee50b55e2857ac3a10
jisunchung <syi06117@gmail.com>
2023-01-16 22:45:42 +0900

    feat : returnPlaces

--- Screen/Detail.js
+++ Screen/Detail.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { Linking, Dimensions, ScrollView } from "react-native";
 import MapView, { Marker } from "react-native-maps";
@@ -13,6 +20,16 @@ function Detail({}) {
     longitude: 0,
     latitudeDelta: 0.1,
     longitudeDelta: 0.1,
+  });
+  const [beaches, setBeaches] = useState([]);
+  const [mountains, setMountains] = useState([]);
+  const [rivers, setRivers] = useState([]);
+  const [valleys, setValleys] = useState([]);
+  const [place, setPlace] = useState({
+    beaches: [],
+    mountains: [],
+    rivers: [],
+    valleys: [],
   });
   const route = useRoute();
   const cityDetails = async () => {
@@ -39,6 +56,16 @@ function Detail({}) {
       });
       console.log("city place :", res.data);
       console.log("palces ", Object.keys(res.data));
+      setPlace({
+        beaches: res.data["beaches"],
+        mountains: res.data["mountains"],
+        rivers: res.data["rivers"],
+        valleys: res.data["valleys"],
+      });
+      setBeaches(res.data["beaches"]);
+      console.log("------------------------beaches", res.data["beaches"]);
+
+      //   setPlace(Object.keys(res.data));
     } catch (error) {
       console.log(error);
     } finally {
@@ -49,6 +76,26 @@ function Detail({}) {
     getPlace();
   }, []);
 
+  const returnPlaces = Object.keys(place).map((place_type) => (
+    // <ScrollView horizontal={true} style={styles.scrollView_horizontal}>
+    <View style={styles.place_block}>
+      {place[`${place_type}`].length != 0 ? (
+        <Text style={styles.place_type_text}>{place_type}</Text>
+      ) : null}
+
+      {place[`${place_type}`].map((place_result, index) => (
+        <TouchableOpacity
+          style={styles.place}
+          onPress={() => {
+            Linking.openURL(place_result["link"]);
+          }}
+        >
+          <Text key={index}>{place_result["name"]}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    // </ScrollView>
+  ));
   if (cityDetailResult != null) {
     return (
       <ScrollView>
@@ -78,6 +125,7 @@ function Detail({}) {
               }}
             />
           </MapView>
+          {returnPlaces}
         </View>
       </ScrollView>
     );
@@ -94,13 +142,14 @@ const styles = StyleSheet.create({
   block: {
     paddingHorizontal: 20,
     paddingVertical: 20,
+    flex: 1,
   },
   text: {
     // padding: 16,
     fontSize: 24,
   },
   image: {
-    width: 300,
+    width: screenWidth - 40,
     height: 300,
   },
   link: {
@@ -109,6 +158,45 @@ const styles = StyleSheet.create({
   map: {
     width: screenWidth - 40,
     height: 300,
+  },
+  scrollView_horizontal: {
+    width: screenWidth - 40,
+    // height: "100%",
+    backgroundColor: "red",
+  },
+  place_block: {
+    backgroundColor: "powderblue",
+    margin: 10,
+    // flexDirection: "row",
+    position: "relative",
+    alignItems: "flex-start",
+    // justifyContent: "center",
+    // width: screenWidth - 40,
+    // height: "auto",
+  },
+  place_type_text: {
+    margin: 5,
+    alignSelf: "center",
+    backgroundColor: "steelblue",
+  },
+  place: {
+    // width: ,
+    // backgroundColor: "skyblue",
+    // padding: 20,
+    // margin: 10,
+    // flexDirection: "column",
+    // flexWrap: "wrap",
+    // borderRadius: 20,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: "oldlace",
+    // alignSelf: "flex-start",
+    // flexDirection: "row",
+    marginHorizontal: "1%",
+    marginBottom: 6,
+    // minWidth: "48%",
+    // textAlign: "center",
   },
 });
 
