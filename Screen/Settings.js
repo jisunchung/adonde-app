c6d494d3c9bb3ced414c15c365c30c9bac257a10
jisunchung <syi06117@gmail.com>
2023-01-26 22:32:37 +0900

    feat : add setting page

--- Screen/Settings.js
+++ Screen/Settings.js
@@ -1,18 +1,63 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, FlatList, TouchableOpacity } from "react-native";
+import { Divider, Text, Switch } from "react-native-paper";
+import { MaterialIcons } from "@expo/vector-icons";
 
 function Settings() {
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
   return (
     <View style={styles.block}>
-      <Text style={styles.text}>settingScreen</Text>
+      {/* <Text style={styles.text}>settingScreen</Text> */}
+      <View style={styles.settingList_block}>
+        <TouchableOpacity>
+          <View style={styles.settingList_action}>
+            <Text style={styles.settingList_text}>언어세팅</Text>
+            <MaterialIcons
+              name="keyboard-arrow-right"
+              size={24}
+              color="black"
+            />
+          </View>
+        </TouchableOpacity>
+        <Divider />
+        <View style={styles.settingList_action}>
+          <Text style={styles.settingList_text}>다크모드</Text>
+          <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+        </View>
+        <Divider />
+        <TouchableOpacity>
+          <Text style={styles.settingList_text}>광고등록</Text>
+        </TouchableOpacity>
+        <Divider />
+        <TouchableOpacity>
+          <Text style={styles.settingList_text}>로그아웃</Text>
+        </TouchableOpacity>
+        <Divider />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  block: {},
+  block: {
+    flex: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+  },
+  settingList_block: {
+    marginTop: 20,
+  },
+  settingList_action: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  settingList_text: {
+    fontSize: 20,
+    margin: 20,
+  },
   text: {
-    padding: 16,
     fontSize: 24,
   },
 });
